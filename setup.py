43339ad5da0c3fe7220f08cd6990f0fa7aa014b4
glowfi <86939591+glowfi@users.noreply.github.com>
2021-12-02 00:24:56 +0530

    cleaned up

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
-from setuptools import setup, find_packages
+import setuptools
 
-classifiers = [
-    "Development Status :: 4 - Beta",
-    "Operation System :: Linux",
-    "Intended Audience :: End Users/Desktop",
-    "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-    "Programming Language :: Python :: 3.9.9",
-]
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-setup(
+setuptools.setup(
     name="xhibit",
     version="0.0.1",
-    description="A python script to exhibit your ascii arts and sytem specs",
-    long_description=open("README.md").read() + "\n\n" + open("CHANGELOG.txt").read(),
-    url="git clone https://github.com/glowfi/xhibit",
     author="glowfi",
-    author_email="",
-    license="GPLv3+",
-    classifiers=classifiers,
-    keywords="system-information ascii-art commandline",
-    packages=find_packages(),
+    description="A python script to exhibit your ascii arts and sytem specs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    url="https://github.com/glowfi/xhibit",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Operating System :: POSIX :: Linux",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.6",
     install_requires=["tcolorpy"],
 )
