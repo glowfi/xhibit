e33cdf7023cd2389fbca7a72d22365120440f95a
glowfi <86939591+glowfi@users.noreply.github.com>
2022-10-30 21:57:23 +0530

    added fixes

--- Exhibition/shell.sh
+++ Exhibition/shell.sh
@@ -2,15 +2,199 @@
 
 # Get distro name
 distroName() {
-	os=$(uname -o)
-	case ${os} in
-	Android)
-		distro="Android"
+	kernel_name=$(uname -s)
+	case $kernel_name in
+
+	Linux | GNU*)
+		os=Linux
 		;;
+
 	*)
-		distro=$(source /etc/os-release && printf '%s\n' "${PRETTY_NAME}")
+		printf '%s\n' "Unknown OS detected: '$kernel_name', aborting..." >&2
+		printf '%s\n' "Open an issue on GitHub to add support for your OS." >&2
+		exit 1
 		;;
 	esac
+
+	[[ $distro ]] && return
+
+	case $os in
+	Linux | BSD | MINIX)
+		if [[ -f /bedrock/etc/bedrock-release && -z $BEDROCK_RESTRICT ]]; then
+			case $distro_shorthand in
+			on | tiny) distro="Bedrock Linux" ;;
+			*) distro=$(</bedrock/etc/bedrock-release) ;;
+			esac
+
+		elif [[ -f /etc/redstar-release ]]; then
+			case $distro_shorthand in
+			on | tiny) distro="Red Star OS" ;;
+			*) distro="Red Star OS $(awk -F'[^0-9*]' '$0=$2' /etc/redstar-release)" ;;
+			esac
+
+		elif [[ -f /etc/armbian-release ]]; then
+			. /etc/armbian-release
+			distro="Armbian $DISTRIBUTION_CODENAME (${VERSION:-})"
+
+		elif [[ -f /etc/siduction-version ]]; then
+			case $distro_shorthand in
+			on | tiny) distro=Siduction ;;
+			*) distro="Siduction ($(lsb_release -sic))" ;;
+			esac
+
+		elif [[ -f /etc/mcst_version ]]; then
+			case $distro_shorthand in
+			on | tiny) distro="OS Elbrus" ;;
+			*) distro="OS Elbrus $(</etc/mcst_version)" ;;
+			esac
+
+		elif type -p pveversion >/dev/null; then
+			case $distro_shorthand in
+			on | tiny) distro="Proxmox VE" ;;
+			*)
+				distro=$(pveversion)
+				distro=${distro#pve-manager/}
+				distro="Proxmox VE ${distro%/*}"
+				;;
+			esac
+
+		elif type -p lsb_release >/dev/null; then
+			case $distro_shorthand in
+			on) lsb_flags=-si ;;
+			tiny) lsb_flags=-si ;;
+			*) lsb_flags=-sd ;;
+			esac
+			distro=$(lsb_release "$lsb_flags")
+
+		elif [[ -f /etc/os-release ||
+			-f /usr/lib/os-release ||
+			-f /etc/openwrt_release ||
+			-f /etc/lsb-release ]]; then
+
+			# Source the os-release file
+			for file in /etc/lsb-release /usr/lib/os-release \
+				/etc/os-release /etc/openwrt_release; do
+				source "$file" && break
+			done
+
+			# Format the distro name.
+			case $distro_shorthand in
+			on) distro="${NAME:-${DISTRIB_ID}} ${VERSION_ID:-${DISTRIB_RELEASE}}" ;;
+			tiny) distro="${NAME:-${DISTRIB_ID:-${TAILS_PRODUCT_NAME}}}" ;;
+			off) distro="${PRETTY_NAME:-${DISTRIB_DESCRIPTION}} ${UBUNTU_CODENAME}" ;;
+			esac
+
+		elif [[ -f /etc/GoboLinuxVersion ]]; then
+			case $distro_shorthand in
+			on | tiny) distro=GoboLinux ;;
+			*) distro="GoboLinux $(</etc/GoboLinuxVersion)" ;;
+			esac
+
+		elif [[ -f /etc/SDE-VERSION ]]; then
+			distro="$(</etc/SDE-VERSION)"
+			case $distro_shorthand in
+			on | tiny) distro="${distro% *}" ;;
+			esac
+
+		elif type -p crux >/dev/null; then
+			distro=$(crux)
+			case $distro_shorthand in
+			on) distro=${distro//version/} ;;
+			tiny) distro=${distro//version*/} ;;
+			esac
+
+		elif type -p tazpkg >/dev/null; then
+			distro="SliTaz $(</etc/slitaz-release)"
+
+		elif type -p kpt >/dev/null &&
+			type -p kpm >/dev/null; then
+			distro=KSLinux
+
+		elif [[ -d /system/app/ && -d /system/priv-app ]]; then
+			distro="Android $(getprop ro.build.version.release)"
+
+		# Chrome OS doesn't conform to the /etc/*-release standard.
+		# While the file is a series of variables they can't be sourced
+		# by the shell since the values aren't quoted.
+		elif [[ -f /etc/lsb-release && $(</etc/lsb-release) == *CHROMEOS* ]]; then
+			distro='Chrome OS'
+
+		elif type -p guix >/dev/null; then
+			case $distro_shorthand in
+			on | tiny) distro="Guix System" ;;
+			*) distro="Guix System $(guix -V | awk 'NR==1{printf $4}')" ;;
+			esac
+
+		# Display whether using '-current' or '-release' on OpenBSD.
+		elif [[ $kernel_name = OpenBSD ]]; then
+			read -ra kernel_info <<<"$(sysctl -n kern.version)"
+			distro=${kernel_info[*]:0:2}
+
+		else
+			for release_file in /etc/*-release; do
+				distro+=$(<"$release_file")
+			done
+
+			if [[ -z $distro ]]; then
+				case $distro_shorthand in
+				on | tiny) distro=$kernel_name ;;
+				*) distro="$kernel_name $kernel_version" ;;
+				esac
+
+				distro=${distro/DragonFly/DragonFlyBSD}
+
+				# Workarounds for some BSD based distros.
+				[[ -f /etc/pcbsd-lang ]] && distro=PCBSD
+				[[ -f /etc/trueos-lang ]] && distro=TrueOS
+				[[ -f /etc/pacbsd-release ]] && distro=PacBSD
+				[[ -f /etc/hbsd-update.conf ]] && distro=HardenedBSD
+			fi
+		fi
+
+		if [[ $(</proc/version) == *Microsoft* || $kernel_version == *Microsoft* ]]; then
+			windows_version=$(wmic.exe os get Version)
+			windows_version=$(trim "${windows_version/Version/}")
+
+			case $distro_shorthand in
+			on) distro+=" [Windows $windows_version]" ;;
+			tiny) distro="Windows ${windows_version::2}" ;;
+			*) distro+=" on Windows $windows_version" ;;
+			esac
+
+		elif [[ $(</proc/version) == *chrome-bot* || -f /dev/cros_ec ]]; then
+			[[ $distro != *Chrome* ]] &&
+				case $distro_shorthand in
+				on) distro+=" [Chrome OS]" ;;
+				tiny) distro="Chrome OS" ;;
+				*) distro+=" on Chrome OS" ;;
+				esac
+			distro=${distro## on }
+		fi
+
+		# distro=$(trim_quotes "$distro")
+		distro=${distro/NAME=/}
+
+		# Get Ubuntu flavor.
+		if [[ $distro == "Ubuntu"* ]]; then
+			case $XDG_CONFIG_DIRS in
+			*"studio"*) distro=${distro/Ubuntu/Ubuntu Studio} ;;
+			*"plasma"*) distro=${distro/Ubuntu/Kubuntu} ;;
+			*"mate"*) distro=${distro/Ubuntu/Ubuntu MATE} ;;
+			*"xubuntu"*) distro=${distro/Ubuntu/Xubuntu} ;;
+			*"Lubuntu"*) distro=${distro/Ubuntu/Lubuntu} ;;
+			*"budgie"*) distro=${distro/Ubuntu/Ubuntu Budgie} ;;
+			*"cinnamon"*) distro=${distro/Ubuntu/Ubuntu Cinnamon} ;;
+			esac
+		fi
+		;;
+
+	esac
+
+	distro=${distro//Enterprise Server/}
+
+	[[ $distro ]] || distro="$os (Unknown)"
+    dist=$(echo "$distro" | tr -d '"')
+
 }
 
 # Get system architecture
@@ -265,7 +449,7 @@ getUptime
 get_cpu
 get_gpu
 
-echo "OS : $distro $arch [$varInit]"
+echo "OS : $dist $arch [$varInit]"
 echo "Kernel : $kernel"
 echo "Packages : $varPkg"
 echo "Shell : $shell"
